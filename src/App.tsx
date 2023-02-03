66773c2037d488596c10d22b7ccc18ba8dd54b61
Intaek Hwang <intaek.hwang@medistream.co.kr>
2023-02-03 17:24:41 +0900

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Board } from "./@core/types/type";
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0);
   const [boards, setBoards] = useState<Board[]>([]);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const { data } = await axios.get<Board[]>(
+        "http://localhost:3030/articles"
+      );
+      console.log(data);
+      setBoards(data);
+    };
+
+    getArticles();
+  }, []);
 
   return (
     <div className="App">
